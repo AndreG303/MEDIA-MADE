0a4b7187dda7f72255583beac52c4c6a4d3d7847
Katelynn <ktlnn.nguyen@gmail.com>
2020-10-31 12:37:33 -0400

    organizing src folder

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+
+class LoginForm extends Component {
+  // Setting the component's initial state
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    console.log(event);
+    event.preventDefault();
+
+    if(this.state.email.length === 0 ){
+      alert("Please enter a valid email");
+    }
+    else if(this.state.password.length < 6){
+      alert("Your password has to be more than 6 characters");
+    }
+    else{
+    //clearing the inputs
+      this.setState({
+        email: "",
+        password: ""
+      });
+    }
+    
+  };
+
+  render() {
+    // Notice how each input has a `value`, `name`, and `onChange` prop
+    return (
+      <div>
+        <p>
+          Hello {this.state.email} 
+        </p>
+        <form className="form">
+          <input
+            value={this.state.email}
+            name="email"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            value={this.state.password}
+            name="password"
+            onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password Here"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
