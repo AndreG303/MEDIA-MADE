41f100aa73f282a8fea6e663dba6c3899b6d6dd4
Ashley Holanda <aholanda12@gmail.com>
2020-11-05 20:14:22 -0500

    added css and rendered the first image

--- client/src/components/OutfitCard/OutfitCard.js
+++ client/src/components/OutfitCard/OutfitCard.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Col, Row, Container } from "../Grid";
+import "./OutfitCard.css";
 
-function OutfitPage(){
+function OutfitPage(props){
+    console.log(props.showOutfits[0].items[0].image)
 
     return(
         <>
         <h1>OUTFIT PAGE</h1>
-        </>
+        <div>
+        <Container className="outfitCont">
+            <Row>
+                <Col size="md-6">
+                <img className="itemImg" src={process.env.PUBLIC_URL + props.showOutfits[0].items[0].image}></img>
+                </Col>
+            </Row>
+        </Container>
+        </div>
+</>
     )
 }
 
--- client/src/pages/Outfits.js
+++ client/src/pages/Outfits.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import OutfitPage from "../components/OutfitCard/OutfitCard";
+import { useAllOutfits } from "../utils/OutfitAPICalls";
 
 function Outfits(){
-
+    const [showOutfits] = useAllOutfits();
     return(
         <>
-        <OutfitPage />
+        { (showOutfits.length > 0) ? <OutfitPage showOutfits={showOutfits}/> : null}
         </>
     )
 }
