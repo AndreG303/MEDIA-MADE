91767e48f8c264c2e41bebfb85d38976a4f4000f
Elizabeth Lee <dlekwjd347@gmail.com>
2020-11-07 11:38:01 -0500

    added Typed function

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -5,12 +5,14 @@ import React, {useState, useEffect} from "react";
 import SingleOutfit from "../components/SingleOutfit/SingleOutfit"
 import { useAllOutfits } from "../utils/OutfitAPICalls";
 //import outfit from "./Outfitpage";
+import Typed from "../components/Typed/Typed"
 
 function Outfit() {
     const [showOutfits] = useAllOutfits();
     console.log(showOutfits);
     return (
         <>
+        <Typed />
         {/* {showOutfits.map( outfit => <SingleOutfit outfit={outfit} />) } */}
         { (showOutfits.length > 0) ? <SingleOutfit showOutfits={showOutfits}/> : null}
         </>
