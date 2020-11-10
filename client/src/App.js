199a5c0c914fdd1ab0df038209ae457b3861c59e
Ashley Holanda <aholanda12@gmail.com>
2020-11-09 19:15:04 -0500

    created functionality to render outfit card underneath carousal

--- client/src/App.js
+++ client/src/App.js
@@ -38,10 +38,10 @@ function App() {
             <Route exact path="/logout">
               <Logout />
             </Route>
-            <Route exact path="/outfits">
+            <Route exact path={["/outfits", "/outfits/:outfitid"]}>
               <Outfits />
             </Route>
-            <Route exact path="/outfits/:outfitid">
+            <Route exact path="/outfitsdetail/:outfitid">
               <OutfitPage />
             </Route>
             <Route>
