d4150b337a4ce988420a8173d81087d1788458ae
Katelynn <ktlnn.nguyen@gmail.com>
2020-11-10 19:27:23 -0500

    fixing closet routing

--- client/src/pages/OutfitPage.js
+++ client/src/pages/OutfitPage.js
@@ -11,6 +11,27 @@ function OutfitPage(props) {
         items: []
     });
 
+    let styles ={
+        outfitCard:{
+            backgroundColor: "#d8dbe2",
+            color: "white",
+            padding: "40px",
+            backgroundSize: "cover"
+        },
+        outfitImg:{
+            padding: "20px",
+            float: "right"
+        },
+        outfitText:{
+            fontFamily: "'Rubik', sans-serif",
+            fontSize: "25px",
+            float: "right"
+        },
+        emily:{
+            float: "left"    
+        }
+    }
+
     const { outfitid } = useParams();
 
     useEffect(() => {
@@ -34,23 +55,22 @@ function OutfitPage(props) {
     }, [props])
 
     return (
-        <>
-            <p style={{ color: "white" }}>Outfit Card</p>
-            <Container>
-                <img src={outfit.outfitImage} alt="outfit-image" />
+        <div>
+            <Container style={styles.outfitCard}>
+                <img style={styles.emily} src={outfit.outfitImage} alt="outfit-image" />
                 {outfit.items.map((items) =>
                     <Row>
                         <Col size="md-3">
-                            <img className="itemImg" src={process.env.PUBLIC_URL + items.image}></img>
+                            <img style={styles.outfitImg} className="itemImg" src={process.env.PUBLIC_URL + items.image}></img>
                         </Col>
                         <Col size="md-8">
-                            <p className="itemName">{items.name}</p>
-                            <p className="itemLink"><a href={items.url}>Click here to buy this product</a></p>
+                            <p style={styles.outfitText} className="itemName">{items.name}</p>
+                            <p style={styles.outfitText} className="itemLink"><a href={items.url}>Click here to buy this product</a></p>
                         </Col>
                     </Row>
-                )})
-        </Container>
-        </>
+                )}
+            </Container>
+        </div>
     )
 }
 
