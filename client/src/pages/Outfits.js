eff1100cb67ff3ba9ec7d18125fc6c5f6164cee6
Andrea Gamez <andreamgamez@gmail.com>
2020-11-11 10:07:22 -0800

    added route for contact us in footer

--- client/src/pages/Outfits.js
+++ client/src/pages/Outfits.js
@@ -6,6 +6,7 @@ import { useAllOutfits } from "../utils/OutfitAPICalls";
 import { useParams } from "react-router-dom";
 import {Button} from "react-bootstrap";
 import API from "../utils/API";
+import Jumbotron from "../components/Jumbotron2.js/Jumbotron2";
 
 
 
@@ -29,13 +30,15 @@ function Outfits(props) {
 
     return (
         <>
+        <Jumbotron />
             {(showOutfits.length > 0) ? <SingleOutfit showOutfits={showOutfits} handleChangeOfPage={handleChangeOfPage} /> : null}
             { showOutfitBottom && (
                 <OutfitPage outfitid={showOutfitBottom} />
                 
             )}
            
-
+           
+        
             <TopScrollBtn />
         </>
     )
