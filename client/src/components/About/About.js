24b5e4aa058b5f2523ba887c22d61d7d520b3c71
Elizabeth Lee <dlekwjd347@gmail.com>
2020-11-10 21:05:18 -0500

    removed container background color

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./About.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function About(){
     return (
@@ -9,14 +8,10 @@ function About(){
   <div className="card">
     <img src="https://avatars2.githubusercontent.com/u/64981290?s=460&u=bd1bebd713cf9b54d11eb1aadd4fc869c0ffa520&v=4" alt="Elizabeth Lee" className="card__image"></img>
     <p className="card__name">Elizabeth Lee</p>
-    <div className="grid-container">
+    <div>
 
-      <div className="grid-child-posts">
-        156 Post
-      </div>
-
-      <div className="grid-child-followers">
-        1012 Likes
+      <div className="usTitle">
+      Front-end, Back-end, &amp; Database
       </div>
 
     </div>
@@ -33,10 +28,10 @@ function About(){
   <div className="card">
     <img src="https://avatars0.githubusercontent.com/u/65183415?s=460&u=e69995076940955d1d2c83d9e4bea2908a239592&v=4" alt="Andrea Gamez" className="card__image"></img>
     <p className="card__name">Andrea Gamez</p>
-    <div className="grid-container">
+    <div>
 
-      <div className="">
-        305 Post
+      <div className="usTitle">
+      Project Manager &amp; Front-end
       </div>
 
     </div>
@@ -52,15 +47,12 @@ function About(){
   <div className="card">
     <img src="https://avatars3.githubusercontent.com/u/61949164?s=460&u=46ac2bb3b962ee705bb9d7f7fb9fd8d5a8db42d9&v=4" alt="Ashley Holanda" className="card__image"></img>
     <p className="card__name">Ashley Holanda</p>
-    <div className="grid-container">
+    <div>
 
-      <div className="grid-child-posts">
-        902 Post
+      <div className="usTitle">
+      Back-end &amp; authentication
       </div>
 
-      <div className="grid-child-followers">
-        1300 Likes
-      </div>
 
     </div>
     <ul className="social-icons">
@@ -75,15 +67,12 @@ function About(){
   <div className="card">
     <img src="https://avatars1.githubusercontent.com/u/64226204?s=460&u=44dd0fe627b9f38d703dd37a2b20e94edd248cb0&v=4" alt="Katelynn Nguyen" className="card__image"></img>
     <p className="card__name">Katelynn Nguyen</p>
-    <div className="grid-container">
+    <div>
 
-      <div className="grid-child-posts">
-        902 Post
+      <div className="usTitle">
+      Back-end &amp; Digital drawing
       </div>
 
-      <div className="grid-child-followers">
-        1300 Likes
-      </div>
 
     </div>
     <ul className="social-icons">
