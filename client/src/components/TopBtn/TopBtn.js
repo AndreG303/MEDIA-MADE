a1b261fa43c4873d7c9fe6d28e863adbb6e4c07b
Katelynn <ktlnn.nguyen@gmail.com>
2020-11-11 18:30:31 -0500

    adding box shadows to img on cards

--- client/src/components/TopBtn/TopBtn.js
+++ client/src/components/TopBtn/TopBtn.js
@@ -1,26 +1,31 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../../app.css";
-import { FaLongArrowAltUp } from 'react-icons/fa';
+import { FaLongArrowAltUp } from "react-icons/fa";
 
-function TopBtn () {
-    const [showScroll, setShowScroll] = useState(false)
-const checkScrollTop = () => {    
-   if (!showScroll && window.pageYOffset > 400){
-      setShowScroll(true)    
-   } else if (showScroll && window.pageYOffset <= 400){
-      setShowScroll(false)    
-   }  
-};
-const scrollTop = () =>{
-    window.scrollTo({top: 0, behavior: 'smooth'});
+function TopBtn() {
+  const [showScroll, setShowScroll] = useState(false);
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  window.addEventListener('scroll', checkScrollTop)
+  useEffect(() => {
+  window.addEventListener("scroll", checkScrollTop);
+  });
 
   return (
-        <FaLongArrowAltUp className="scrollTop" onClick={scrollTop} style={{height: 40, display: showScroll ? 'flex' : 'none'}}/>
+    <FaLongArrowAltUp
+      className="scrollTop"
+      onClick={scrollTop}
+      style={{ height: 40, display: showScroll ? "flex" : "none" }}
+    />
   );
-
 }
 
 export default TopBtn;
