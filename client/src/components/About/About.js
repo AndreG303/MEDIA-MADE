8e1c3b00150ec70471c9e707d351eaecce3c8430
Elizabeth Lee <dlekwjd347@gmail.com>
2020-11-11 10:42:49 -0500

    changed class names

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -4,8 +4,8 @@ import "./About.css";
 function About(){
     return (
     <>
-<div className="container">
-  <div className="card">
+<div className="aboutContainer">
+  <div className="personCard">
     <img src="https://avatars2.githubusercontent.com/u/64981290?s=460&u=bd1bebd713cf9b54d11eb1aadd4fc869c0ffa520&v=4" alt="Elizabeth Lee" className="card__image"></img>
     <p className="card__name">Elizabeth Lee</p>
     <div>
@@ -22,7 +22,7 @@ function About(){
     <a href="mailto:andreamgamez@gmail.com"><button className="btn draw-border">Message</button></a>
 
   </div>
-  <div className="card">
+  <div className="personCard">
     <img src="https://avatars0.githubusercontent.com/u/65183415?s=460&u=e69995076940955d1d2c83d9e4bea2908a239592&v=4" alt="Andrea Gamez" className="card__image"></img>
     <p className="card__name">Andrea Gamez</p>
     <div>
@@ -38,7 +38,7 @@ function About(){
     </ul>
     <a href="mailto:dlekwjd347@gmail.com"><button className="btn draw-border">Message</button></a>
   </div>
-  <div className="card">
+  <div className="personCard">
     <img src="https://avatars3.githubusercontent.com/u/61949164?s=460&u=46ac2bb3b962ee705bb9d7f7fb9fd8d5a8db42d9&v=4" alt="Ashley Holanda" className="card__image"></img>
     <p className="card__name">Ashley Holanda</p>
     <div>
@@ -55,7 +55,7 @@ function About(){
     </ul>
     <a href="mailto:dlekwjd347@gmail.com"><button className="btn draw-border">Message</button></a>
   </div>
-  <div className="card">
+  <div className="personCard">
     <img src="https://avatars1.githubusercontent.com/u/64226204?s=460&u=44dd0fe627b9f38d703dd37a2b20e94edd248cb0&v=4" alt="Katelynn Nguyen" className="card__image"></img>
     <p className="card__name">Katelynn Nguyen</p>
     <div>
