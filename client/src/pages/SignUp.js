f226aabd0bf5b3fc498d2ad8d94f7cd7f55a2ed5
Katelynn <ktlnn.nguyen@gmail.com>
2020-12-03 17:03:24 -0500

    adding conditional render to add to closet button

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -7,24 +7,13 @@ import { Container } from "react-bootstrap";
 function SignUp(props) {
   const { loggedIn } = useContext(UserContext);
 
-  let styles = {
 
-    p: {
-      color: "#d8dbe2",
-      textAlign: "center",
-      fontFamily: "'Rubik', sans-serif",
-      marginBottom: "30px",
-    },
-    container: {
-      backgroundColor: "rgba(0,0,255,0)",
-    },
-  };
 
   return (
-    <Container style={styles.container}>
+    <Container>
       {loggedIn && <Redirect to="/" />}
       <h1 className="form-header">Sign-Up</h1>
-      <p style={styles.p}>
+      <p className="sign-up-text">
         Create your account. It's free and only takes one minute.
       </p>
       <SignUpForm />
